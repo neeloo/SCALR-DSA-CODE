df7e57dcf4dcec5f7e22b81e032824ff7fe25290
Nilu patel <nilusingh945164@gmail.com>
2024-03-22 10:47:57 +0530

    sum

---
+++ SumOfRange.java
@@ -0,0 +1,19 @@
+public class SumOfRange {
+    public static void main(String[] args) {
+        
+        int arr[]={3,6,2,4,5,2,8,-9,3,1};
+        int sum=0; 
+        int l=6;
+        int r=9;
+        int k=5;
+        for(int i=0;i<=k; i++){
+            for(int j=l;j<=r;j++){
+                sum+=arr[j];
+            }
+            System.out.println(sum);
+            return;
+        }
+       
+    }
+    
+}
