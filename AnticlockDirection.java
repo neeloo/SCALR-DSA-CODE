07dac08b63359980cce3539285aec6f00446d630
Nilu patel <nilusingh945164@gmail.com>
2024-03-22 09:44:15 +0530

    clock

--- AnticlockDirection.java
+++ AnticlockDirection.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class AnticlockDirection {
     
 
@@ -9,9 +12,10 @@ public class AnticlockDirection {
             rotateArray(arr, k);
     
             // Print the rotated array
-            for (int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i] + " ");
-            }
+            // for (int i = 0; i < arr.length; i++) {
+            //     System.out.print(arr[i] + " ");
+            // }
+            System.out.println(Arrays.toString(arr));
         }
     
         public static void rotateArray(int[] arr, int k) {
@@ -32,7 +36,7 @@ public class AnticlockDirection {
             for (int i = 0; i < k; i++) {
                 arr[arr.length - k + i] = temp[i];
             }
-            
+
         }
     }
     
